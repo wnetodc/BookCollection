76a1de7e17a8864af0f608d5efae01e02897fcc5
wellington neto <wellington.neto@gmail.com>
2017-04-14 16:51:51 -0300

    versão 1
    aplicativo salvando/atualizando e deletando do rolo da camera

--- BookCollection/AddBookViewController.swift
+++ BookCollection/AddBookViewController.swift
@@ -8,19 +8,53 @@
 
 import UIKit
 
-class AddBookViewController: UIViewController {
+class AddBookViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     @IBOutlet var livroImageView: UIImageView!
     @IBOutlet var nomeTextField: UITextField!
+    @IBOutlet var addOrUpdateButton: UIButton!
+    @IBOutlet var deleteButton: UIButton!
+    
+    var newBook : Book? = nil
+    var imagePicker =  UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        
+        // verifica se ocorrerá um insert ou um update
+        if newBook != nil {
+            livroImageView.image = UIImage(data: newBook?.image as! Data)
+            nomeTextField.text = newBook?.title
+            
+            addOrUpdateButton.setTitle("Update", for: .normal)
+            
+        }else{
+            deleteButton.isHidden = true
+            
+        }
     }
 
     @IBAction func listarFotos(_ sender: Any) {
         print("photo tapped")
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage?
+        
+        livroImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        
     }
 
     @IBAction func chamarCamera(_ sender: Any) {
@@ -28,7 +62,38 @@ class AddBookViewController: UIViewController {
     }
 
     @IBAction func adicionarButton(_ sender: Any) {
-        print("add tapped")
+        
+        if newBook != nil {
+            //editar
+            newBook!.title = nomeTextField.text
+            newBook!.image = UIImagePNGRepresentation(livroImageView.image!) as NSData?
+        }else{
+            //inserindo
+            let context =  (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let book = Book(context: context)
+            
+            book.title = nomeTextField.text
+            book.image = UIImagePNGRepresentation(livroImageView.image!) as NSData?
+        }
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+        
     }
+    
+    @IBAction func deleteButtonTapped(_ sender: Any) {
+        
+        
+        let context =  (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        context.delete(newBook!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+        
+    }
+    
     
 }
--- BookCollection/ViewController.swift
+++ BookCollection/ViewController.swift
@@ -8,16 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tableView: UITableView!
+    
+    var books : [Book] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let listBooks = books[indexPath.row]
+        
+        cell.textLabel?.text = listBooks.title
+        cell.imageView?.image = UIImage(data: listBooks.image as! Data)
+        
+        return cell
+    }
+    
+    //quando selecionar um livro da lista
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let listBooks = books[indexPath.row]
+        performSegue(withIdentifier: "showBookSegue", sender: listBooks)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextVC = segue.destination as! AddBookViewController
+        nextVC.newBook = sender as? Book
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+            try  books = context.fetch(Book.fetchRequest())
+            tableView.reloadData()
+        } catch  {
+            print("erro")
+        }
+       
     }
 
 
